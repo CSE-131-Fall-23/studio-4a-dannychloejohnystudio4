cbb545f2dd40569b072b2c59de9969cda07af96c
johnyeskandar <johnyeskandar@localhost>
2023-10-05 10:07:03 -0500

    file

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,6 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		int file = in.nextInt();
 	}
 }
