601d65308343a838d8d68b1da7a81bfce42ef7a7
johnyeskandar <johnyeskandar@localhost>
2023-10-12 10:44:24 -0500

    hello

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setXscale(0,100);
+		StdDraw.setYscale(0,100);
+
+		double x = 50;
+		double y = 50;
+		double halfWidth = 30;
+		double halfHeight = 20;
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		StdDraw.setPenColor(Color.RED);
+		double halfH = 10;
+		StdDraw.filledCircle(x, y, halfH);
+		
 	}
+	
+	
 }
